c5f8a6f0385cb9853907c2078e71ec6d770f5e46
Christian <christiancorby@gmail.com>
2020-05-27 15:40:26 -0400

    CRUD Functions

---
+++ medcabinet/src/SaveStrains/savedList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+export default class SavedList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="saved-list">
+        <h3>Saved Strains:</h3>
+        {this.props.list.map(strain => {
+          return (
+            <NavLink
+              to={`/strains/${strain.id}`}
+              key={strain.id}
+              activeClassName="saved-active"
+            >
+              <span className="saved-strain">{strain.strain_name}</span>
+            </NavLink>
+          );
+        })}
+        <div className="home-button">
+          <Link to="/">Home</Link>
+        </div>
+        </div>
+    );
+      }
+    }
